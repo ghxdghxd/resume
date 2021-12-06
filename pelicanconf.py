de2a1b63d754f9c18cb0b6eed6d2d141623bdb76
guojt-4451@163.com <guojt-4451@163.com>
2021-12-06 14:18:09 +0800

    creat theme

---
+++ pelicanconf.py
@@ -0,0 +1,81 @@
+AUTHOR = 'JT GUO'
+SITENAME = 'Resume'
+SITEURL = 'https://ghxdghxd.github.io/resume'
+GITHUB_URL = 'https://github.com/ghxdghxd'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# sections
+TITLE = "厦门大学 健康医疗大数据国家研究院 助理研究员"
+PHONE_NUMBER = "00000000000"
+EMAIL = "guojt-4451@163.com"
+WEBSITE = ""
+LOCATION = "厦门大学"
+INTRUDUCTION = """
+计算医学与生物信息学
+"""
+
+THEME = "themes/resume"
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+# PLACEHOLDER = "好记性不如烂键盘···"
+
+# STATIC_PATHS = ["pages", "pdfs", 'images']
+
+# EXTRA_PATH_METADATA = {
+#     # about.md生成到output下, 表示about.html的路径
+#     'pages/about.md': {'path': 'about.html'},
+#     'images/logo/favicon.ico': {'path': 'favicon.ico'},
+#     'images/logo/user_picture.jpg': {'path': 'user_picture.jpg'},
+# }
+
+# PLUGIN_PATHS = ['plugins']
+# PLUGINS = ['tipue-search', "sitemap", "i18n_subsites", "pelican-algolia-search",
+#            "neighbors", "pelican-show-pdf", "better_codeblock_line_numbering"]
+
+# THEME = "themes/webnote"
+# DIRECT_TEMPLATES = ['index', "solar_system"]
+# # title, link, font-awesome-id(http://fontawesome.io)
+# # MENUITEMS = [('主页', '.', 'fa-home'),
+# #              ('归档', 'archives.html', 'fa-archive'),
+# #              ("关于", "pages/about.html", "fa-user")]
+
+# JINJA_ENVIRONMENT = {
+#     'extensions': ['jinja2.ext.i18n'],
+# }
+
+# DISPLAY_CATEGORIES_ON_MENU = True  # 显示分类
+
+
+# # PAGINATED_DIRECT_TEMPLATES = ['index']  # 分页
+# # DEFAULT_PAGINATION = 5
+
+# # Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# MARKDOWN = {
+#     'extension_configs': {
+#         'markdown.extensions.codehilite': {'css_class': 'highlight','linenums': 'False'},
+#         'markdown.extensions.extra': {},
+#         'markdown.extensions.meta': {},
+#     },
+#     'output_format': 'html5',
+# }
